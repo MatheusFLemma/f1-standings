8aa1d93118cdbf29f20397e8966c8c06af143033
Matheus Favaro Lemma <matheusflemma123@gmail.com>
2022-02-15 10:52:56 -0300

    feat: add header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="https://www.formula1.com/etc/designs/fom-website/images/f1_logo.svg"
+        alt="Formula One logo"
+      />
+    </header>
+  );
+}
