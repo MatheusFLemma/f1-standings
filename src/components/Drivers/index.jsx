683bf2d8feb3f74d3bb7c89227e76f645bba5dfa
Matheus Favaro Lemma <matheusflemma123@gmail.com>
2022-02-16 11:23:44 -0300

    style: add style in drivers component

--- src/components/Drivers/index.jsx
+++ src/components/Drivers/index.jsx
@@ -2,25 +2,25 @@ export function Drivers({ data }) {
   const columns = data[0] && Object.keys(data[0]);
 
   return (
-    <section className="last-result">
-      <h2 className="last-result__title">F1 Last Result</h2>
-      <table className="last-result__table">
+    <section className="drivers">
+      <h2 className="drivers__title">F1 Last Result</h2>
+      <table className="drivers__table">
         <thead>
-          <tr className="last-result__head">
+          <tr className="drivers__head">
             {data[0] &&
               columns.map((heading) => (
-                <th key={heading} className="last-result__heading">
+                <th key={heading} className="drivers__heading">
                   {heading}
                 </th>
               ))}
           </tr>
         </thead>
 
-        <tbody className="last-result__body">
+        <tbody className="drivers__body">
           {data.map((row) => (
-            <tr key={Math.random()} className="last-result__body-row">
+            <tr key={Math.random()} className="drivers__body-row">
               {columns.map((column) => (
-                <td key={Math.random()} className="last-result__body-data">
+                <td key={Math.random()} className="drivers__body-data">
                   {row[column]}
                 </td>
               ))}
