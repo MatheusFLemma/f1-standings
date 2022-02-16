95d4cd33959b464663c55aba6a4c8ecb524bc24e
Matheus Favaro Lemma <matheusflemma123@gmail.com>
2022-02-16 10:35:39 -0300

    feat: add 'Last Result' feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
+// REACT IMPORTS
+import { useEffect, useState } from "react";
+
+// SERVICE IMPORT
+import { api } from "./services/api";
+
+// COMPONENTS IMPORTS
 import { Header } from "./components/Header";
+import { LastResult } from "./components/LastResult";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const getResults = async () => {
+    const dataArray = [];
+    const res = await api.get("/current/last/results.json");
+    const result = res.data.MRData.RaceTable.Races[0].Results;
+
+    try {
+      for (let d of result) {
+        dataArray.push({
+          Pos: <div className="data__center">{d.position}</div>,
+          Nº: <div className="data__center">{d.number}</div>,
+          Driver: (
+            <>
+              {d.Driver.givenName}
+              <p>
+                <strong>{d.Driver.familyName}</strong>
+              </p>
+            </>
+          ),
+          Card: d.Constructor.name,
+          Pts: <div className="data__center">{d.points}</div>,
+        });
+      }
+      setData(dataArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <LastResult data={data} />
+      </div>
     </div>
   );
 }
---
+++ src/components/LastResult/index.jsx
@@ -0,0 +1,33 @@
+export function LastResult({ data }) {
+  const columns = data[0] && Object.keys(data[0]);
+
+  return (
+    <section className="last-result">
+      <h2 className="last-result__title">F1 Last Result</h2>
+      <table className="last-result__table">
+        <thead>
+          <tr className="last-result__head">
+            {data[0] &&
+              columns.map((heading) => (
+                <th key={heading} className="last-result__heading">
+                  {heading}
+                </th>
+              ))}
+          </tr>
+        </thead>
+
+        <tbody className="last-result__body">
+          {data.map((row) => (
+            <tr key={Math.random()} className="last-result__body-row">
+              {columns.map((column) => (
+                <td key={Math.random()} className="last-result__body-data">
+                  {row[column]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
