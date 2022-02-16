adfbc694aa16778432dc0aa68280f2b22228670d
Matheus Favaro Lemma <matheusflemma123@gmail.com>
2022-02-16 11:21:24 -0300

    feat: add 'Drivers standings' feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import { api } from "./services/api";
 // COMPONENTS IMPORTS
 import { Header } from "./components/Header";
 import { LastResult } from "./components/LastResult";
+import { Drivers } from "./components/Drivers";
 
 function App() {
   const [result, setResult] = useState([]);
+  const [drivers, setDrivers] = useState([]);
 
   useEffect(() => {
     getLastResults();
+    getDriver();
   }, []);
 
   const getLastResults = async () => {
@@ -21,10 +24,39 @@ function App() {
     const result = res.data.MRData.RaceTable.Races[0].Results;
 
     try {
-      for (let d of result) {
+      for (let r of result) {
         resultArray.push({
+          Pos: <div className="data__center">{r.position}</div>,
+          Nº: <div className="data__center">{r.number}</div>,
+          Driver: (
+            <>
+              {r.Driver.givenName}
+              <p>
+                <strong>{r.Driver.familyName}</strong>
+              </p>
+            </>
+          ),
+          Card: r.Constructor.name,
+          Pts: <div className="data__center">{r.points}</div>,
+        });
+      }
+      setResult(resultArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDriver = async () => {
+    const driverArray = [];
+    const res = await api.get("/2021/driverStandings.json");
+    const result =
+      res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+
+    try {
+      for (let d of result) {
+        driverArray.push({
           Pos: <div className="data__center">{d.position}</div>,
-          Nº: <div className="data__center">{d.number}</div>,
+          Nº: <div className="data__center">{d.Driver.permanentNumber}</div>,
           Driver: (
             <>
               {d.Driver.givenName}
@@ -33,11 +65,11 @@ function App() {
               </p>
             </>
           ),
-          Card: d.Constructor.name,
+          Card: d.Constructors[0].name,
           Pts: <div className="data__center">{d.points}</div>,
         });
       }
-      setResult(resultArray);
+      setDrivers(driverArray);
     } catch (error) {
       console.log(error);
     }
@@ -48,6 +80,7 @@ function App() {
       <Header />
       <div className="container">
         <LastResult data={result} />
+        <Drivers data={drivers} />
       </div>
     </div>
   );
